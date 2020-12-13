dcfbc6c951ee4d30a0a463278408c2ccdb0b8ce6
lwl <719603766@qq.com>
2020-12-13 22:43:34 +0800

    123

---
+++ src/com/bjpowernode/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Person {
+
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("lala");
+    }
+}
+
